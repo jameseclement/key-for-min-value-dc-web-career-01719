4ba51484fed6f51cbff616cac1f2b97bbbbaa56d
jameseclement <jameseclement@gmail.com>
2019-01-04 15:51:29 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -8,7 +8,7 @@ def key_for_min_value(name_hash)
   
   name_hash.each { |name, number|
   binding.pry
-  if bottom_value == nil || number < bottom_value
+  if bottom_value == 0 || number < bottom_value
     bottom_value = number
     bottom_key = name
   end
