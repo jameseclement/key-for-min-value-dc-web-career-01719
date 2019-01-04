5f532be6960292b7d115ccd931cb12330f66e1ff
jameseclement <jameseclement@gmail.com>
2019-01-04 15:49:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,11 +5,13 @@ def key_for_min_value(name_hash)
   bottom_value = nil
   bottom_key = nil
   
-  name_hash.collect { |name, number|
-  if number < bottom_value
-    number = bottom_value
-    name = bottom_key
+  name_hash.each { |name, number|
+  binding.pry
+  if bottom_value == nil || number < bottom_value
+    bottom_value = number
+    bottom_key = name
   end
   }
+  bottom_key
 
 end
